5e5d50617130df5d7b28d56b10194806e2d6d23a
JuanPitencel <juanpitencel@gmail.com>
2024-08-28 10:34:00 -0300

    CORS

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.modules';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept',
+  });
   await app.listen(3000);
 }
 bootstrap();
